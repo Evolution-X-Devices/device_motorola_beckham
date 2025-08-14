705546aed216f4412f7d228a39d0878956a4fc90
LuK1337 <priv.luk@gmail.com>
2025-08-14 23:47:18 +0200

    beckham: Migrate to AIDL LiveDisplay SDM/sysfs HALs
    Change-Id: I580e0721b050480ed5a9ca8bb145f9ef1f1b174f

--- device.mk
+++ device.mk
@@ -45,6 +45,11 @@ PRODUCT_PACKAGES += \
     init.recovery.beckham.rc \
     init.sys.beckham.rc
 
+# LiveDisplay
+$(call soong_config_set,livedisplay_sdm,enable_dm,false)
+$(call soong_config_set,livedisplay_sysfs,enable_ab,true)
+$(call soong_config_set,livedisplay_sysfs,enable_se,true)
+
 # ModService
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/whitelist_modservice.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/whitelist_modservice.xml
